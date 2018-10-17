a4950e7f6e2f876af858f185ad75eac76f46a432
zoell98 <43257985+zoell98@users.noreply.github.com>
2018-10-17 14:37:53 +0800

    Delete new_f.sh

--- new_f.sh
+++
@@ -1,6 +0,0 @@
-gcc -I ./moduleA -c ./moduleA/student.c -o ./release/student.o
-gcc -I ./moduleA -c ./moduleA/course.c -o ./release/course.o
-ar rsv ./release/libstudent.a ./release/student.o
-ar rsv ./release/libcourse.a ./release/course.o
-gcc main.c -I ./moduleA -L ./release -lstudent -lcourse -o main
-
